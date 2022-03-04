f8320852940743ff527c7cef1dea8c4ca0603306
Mortis66666 <mortisloverbs@gmail.com>
2022-03-04 13:44:41 +0800

    Dead rat image

--- sprites/rat.py
+++ sprites/rat.py
@@ -7,6 +7,7 @@ class Rat(BaseSprite):
 
     def __init__(self, win: pygame.Surface, x, y):
         super().__init__(win, x, y)
+        self.alive = True
 
 
     @property
@@ -19,3 +20,6 @@ class Rat(BaseSprite):
             ),
             (64, 64)
         )
+
+    def __str__(self):
+        return super().__str__() if self.alive else "Dead"
